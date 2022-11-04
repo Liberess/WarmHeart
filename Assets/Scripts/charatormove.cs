6194b63df42c1b7e890fe02cb846ada369fe4445
hth0209 <117443242+hth0209@users.noreply.github.com>
2022-11-04 20:24:03 +0900

    FEAT Player Functions

---
+++ Assets/Scripts/charatormove.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charatormove : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    [SerializeField, Range(0f, 5f)] private float Movespeed;
+    [SerializeField, Range(0f, 5f)] private float Flyforce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigid.AddForce(Vector2.left * Movespeed , ForceMode2D.Impulse);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigid.AddForce(Vector2.right * Movespeed  , ForceMode2D.Impulse);
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rigid.AddForce(Vector2.up * Flyforce, ForceMode2D.Impulse);
+
+        }
+    }
+}
