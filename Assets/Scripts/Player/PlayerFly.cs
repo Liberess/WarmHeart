84e6094327992817d4d975c8e7bde163964588fa
hth0209 <117443242+hth0209@users.noreply.github.com>
2022-11-05 20:15:32 +0900

    fly

--- Assets/Scripts/Player/PlayerFly.cs
+++ Assets/Scripts/Player/PlayerFly.cs
@@ -8,10 +8,13 @@ public class PlayerFly : MonoBehaviour
     [SerializeField, Range(0f, 5f)] private float Flyforce;
     [SerializeField, Range(0f, 100f)] public float FlyPower;
     public int FlyCount=0;
+
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -43,18 +46,20 @@ public class PlayerFly : MonoBehaviour
 
     }
     
-    private void OnCollisionStay2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Ground")
         {
             FlyCount = 0;
+            anim.SetBool("isFly", false);
             rigid.gravityScale = 1f;
         }
     }
-    private void OnCollisionExit2D(Collision2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        if( collision.gameObject.tag == "Ground")
+        if (collision.gameObject.tag == "Ground")
         {
+            anim.SetBool("isFly", true);
             FlyCount = 1;
         }
     }
