27e2a0927e8d1983a63cceb3fb3ca1bbb16dce50
hth0209 <117443242+hth0209@users.noreply.github.com>
2022-11-06 17:32:31 +0900

    ef

--- Assets/Scripts/Obstacle/BreakableWall.cs
+++ Assets/Scripts/Obstacle/BreakableWall.cs
@@ -5,13 +5,30 @@ using UnityEngine;
 public class BreakableWall : Obstacle
 {
     private LivingEntity livingEntity;
-
+    public Sprite Hp30;
+    public Sprite Hp20;
+    public Sprite Hp10;
     private void Start()
     {
         livingEntity = GetComponent<LivingEntity>();
         livingEntity.DeathAction += () => Destroy(gameObject);
     }
-
+    private void Update()
+    {
+        switch (livingEntity.CurrentHp)
+        {
+            case 30:
+                GetComponent<SpriteRenderer>().sprite = Hp30;
+                break;
+            case 20:
+                GetComponent<SpriteRenderer>().sprite = Hp20;
+                break;
+            case 10:
+                GetComponent<SpriteRenderer>().sprite = Hp10;
+                break;
+        }
+        
+    }
     public override void OnEnter()
     {
 
